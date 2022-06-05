b72c0f9767967ee6a42750b8311bb8ba485a795a
shalnsingh@outlook.com <shalnsingh@outlook.com>
2022-06-05 16:30:13 +0530

    updated shop.js

--- routes/shop.js
+++ routes/shop.js
@@ -2,25 +2,11 @@ const path =require('path')
 const express = require('express');
 const req = require('express/lib/request');
 
-const rootDir= require('../util/path')
-const adminData = require('./admin');
+const productsControllers = require('../controllers/products');
 
 const router = express.Router();
 
 
-router.get('/', (req, res, next) => {
-  console.log('array',adminData.products);
-  const products=adminData.products
-  // res.sendFile(path.join(rootDir,'views','shop.html'));
-  res.render('shop',{
-    prods:products,
-    pageTitle:'Shop', 
-    path:'/',
-    hasProducts: products.length >0,
-    activeShop: true,
-    productCSS: true,
-
-  })
-});
+router.get('/', productsControllers.getProducts);
 
 module.exports = router;
